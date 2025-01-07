ed55392380fb74dee49c2eb09cf92b494797983b
William Milanez <william_milanez@hotmail.com>
2025-01-06 21:01:41 -0300

    melhorando o código - conceitos de orientação e objetos

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-import './router.js'
+import { Router } from './router.js'
 
-// const routes = {
-//     "/": "/pages/home.html",
-//     "/the_universe": "/pages/the_universe.html",
-//     "/exploration": "/pages/exploration.html",
-//     404: "/pages/404.html",
-// }
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/the_universe", "/pages/the_universe.html")
+router.add("/exploration", "/pages/exploration.html")
+router.add(404, "/pages/404.html")
 
-// handle()
+router.handle()
 
-// window.onpopstate = () => handle() // serve para monitorar eventos de navegação no histórico do navegador, como os cliques nos botões "voltar" ou "avançar"
-// // O "onpopstate" pode ser usado em:
-// // aplicações que usam navegação baseada em JS, como SPA.
-// // para manter a URL sincronizada com o estado da aplicação.
-// // para manipular conteúdo dinamicamente sem recarregar a página.
+window.onpopstate = () => router.handle() // serve para monitorar eventos de navegação no histórico do navegador, como os cliques nos botões "voltar" ou "avançar"
+// O "onpopstate" pode ser usado em:
+// aplicações que usam navegação baseada em JS, como SPA.
+// para manter a URL sincronizada com o estado da aplicação.
+// para manipular conteúdo dinamicamente sem recarregar a página.
 
-// window.route = () => route()
+window.route = () => router.route()
